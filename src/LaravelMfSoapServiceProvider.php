fda379a0acd219df97b231418facd3a7f655a9a7
Florian Ratz <florian.ratz@supsign.ch>
2020-09-08 16:24:58 +0200

    no message

---
+++ src/LaravelMfSoapServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Supsign\LaravelMfSoap;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMfSoapServiceProvider extends ServiceProvider
+{
+
+    public function boot() 
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
