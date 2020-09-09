216104717416a14013208ada1ff982181ec52698
Florian Ratz <florian.ratz@supsign.ch>
2020-09-09 13:44:51 +0200

    no message

--- src/MyFactorySoapApi.php
+++ src/MyFactorySoapApi.php
@@ -2,13 +2,28 @@
 
 namespace Supsign\LaravelMfSoap;
 
-
+use Config;
 
 class MyFactorySoapApi
 {
+    private
+        $wsdl        = null,
+        $client      = null,
+        $response    = null,
+        $authParams  = array(),
+        $loginParams = array('trace' => 1);
 
-	public function __construct()
-	{
-		var_dump('construct!');
+	public function __construct() {
+		$this->wsdl = env('MF_API_WSDL');
+		$this->loginParams['login'] = env('MF_API_LOGIN');
+		$this->loginParams['password'] = env('MF_API_PASSWORD');
+
+		$this->client = new \SoapClient($this->wsdl, $this->loginParams);
+
+		var_dump($this->client);
 	}
+
+
+
+
 }
